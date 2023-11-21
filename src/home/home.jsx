b8f18c46704b0e209a2ac0bc692c1d1691ad7e59
EderAmorimIFMS <jgfuso@gmail.com>
2023-11-21 10:27:20 -0400

    css home

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,14 +1,23 @@
-import {useState} from "react"
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./style.css";
 
 export default function Home() {
-    const [name, steName] = useState()
-    return (
-        <div>
-            <h1>home</h1>
-            <Link to="/todo">To do List</Link>
-            <p>{name}</p>
-            <input type="text" onChange={(e)=> steName(e.currentTarget.value)}></input>
-        </div>
-    )
+  const [name, steName] = useState();
+  return (
+    <div className="cor homezin">
+      <h1>home</h1>
+      <button className="butao">
+        <Link to="/todo">Entrar</Link>
+      </button>
+      <p>{name}</p>
+      <div className="inputContainer">
+        <input
+          className="inputi"
+          type="text"
+          onChange={(e) => steName(e.currentTarget.value)}
+        />
+      </div>
+    </div>
+  );
 }
